8481740075181434a676fdf69ea13c6dbdc03a5f
Matthew Swain <matt.swain@peopleadmin.com>
2011-05-22 16:31:49 -0400

    Initial release.

---
+++ awkward_turtle.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'awkward_turtle'
+  s.version     = '0.0.2'
+  s.date        = '2011-05-22'
+  s.summary     = "Awkward Turtle!"
+  s.description = "An animated awkward turtle for your terminal"
+  s.authors     = ["Matthew Swain"]
+  s.email       = 'mswain@gmail.com'
+  s.files       = ["lib/awkward_turtle.rb"]
+  s.executables << "awkward_turtle"
+  s.homepage    = 'http://rubygems.org/gems/awkward_turtle'
+end
---
+++ lib/awkward_turtle.rb
@@ -0,0 +1,36 @@
+class AwkwardTurtle
+  @t1 = %q{
+           _
+          / \
+      _  .""". _
+    /` /`_\ /_`\ `\
+     "`|_> _ <_|`"
+       /\_-'-_/\
+      /_/ `)` \_\
+      ""       ""
+  }
+  @t2 = %q{
+           _
+          / \
+       _ .""". _
+      |/`_\ /_`\|
+      "|_> _ <_|"
+       /\_-'-_/\
+       \_\`)`/_/
+        ""   ""
+  }
+
+  # Animates the turtle in the shell
+  def self.animate
+    interrupted = false
+    trap("INT") {interrupted = true}
+    loop do
+      [ @t1, @t2].each do |t|
+        exit if interrupted
+        system "clear"
+        puts t
+        sleep 1
+      end
+    end
+  end
+end
